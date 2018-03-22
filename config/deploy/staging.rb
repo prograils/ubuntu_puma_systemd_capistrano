16a5aa1bf66387f51dc7cdf39657d4edb40953bd
Maciej Litwiniuk <maciej@litwiniuk.net>
2018-03-22 18:53:02 +0100

    Add master.key to linked files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,8 @@ set :stage, :staging # this defines production stage for deployment
 
 set :branch, 'rails52'
 
+set :linked_files, fetch(:linked_files, []).concat(%w{.rbenv-vars config/master.key})
+
 set :deploy_to, "/home/myapp52/app/"
 
 role :app, %w(myapp52@myapp52.prograils.com)
