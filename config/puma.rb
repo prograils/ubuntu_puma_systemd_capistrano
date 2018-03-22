a47a7ba82cabbdb00c571e163b68ac12df4a2909
Maciej Litwiniuk <maciej@litwiniuk.net>
2018-03-22 18:25:05 +0100

    Puma should listen on port only in development

--- config/puma.rb
+++ config/puma.rb
@@ -7,10 +7,6 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
 # Specifies the `environment` that Puma will run in.
 # Defaults to development
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
@@ -48,4 +44,7 @@ if rails_env == 'production'
       # ActiveRecord::Base.establish_connection(db_url)
     end
   end
+elsif rails_env == "development"
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port        ENV.fetch("PORT") { 3000 }
 end
